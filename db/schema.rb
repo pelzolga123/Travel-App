fb9d56af8f8d7fdc5589a87447d9faaffabeb8ba
pelzolga123 <pelzolga123@gmail.com>
2020-02-13 20:40:18 +0200

    Created Deal model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_144242) do
+ActiveRecord::Schema.define(version: 2020_02_13_175544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: :cascade do |t|
+    t.string "date"
+    t.string "deal_name"
+    t.text "description"
+    t.string "price"
+    t.string "location"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_deals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_02_12_144242) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "deals", "users"
 end
